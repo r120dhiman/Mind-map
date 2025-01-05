23b7140436883de2473bd0d313fcd299cdb9442a
r120dhiman <r120dhiman@gmail.com>
2025-01-05 19:14:58 +0530

    Resolved error

--- Routes/user.js
+++ Routes/user.js
@@ -6,7 +6,7 @@ const path=require('path')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(`./Public/ProfileImg/`))
+    cb(null, path.resolve(`./Public/image/`))
   },
   filename: function (req, file, cb) {
     const filename = `${Date.now()}_${file.originalname}`
